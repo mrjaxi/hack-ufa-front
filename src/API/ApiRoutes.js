b2c9adc0cd62ef3559bca265446d95f715a66406
mrjaxi <bobcatoshigu@gmail.com>
2023-11-25 12:02:47 +0500

    api routes

---
+++ src/API/ApiRoutes.js
@@ -0,0 +1,4 @@
+const ApiRoutes = {
+    "BASE": "http://192.168.1.105:5296/",
+
+}
---
+++ src/utils/testData.js
@@ -0,0 +1,71 @@
+const testData = {
+    id: 1,
+    name: "Name",
+    questionCount: 5,
+    progress: 0,
+    currentAnswer: 0,
+    questions: [
+        {
+            id: 1,
+            title: 'Второй вопрос',
+            content: 'Second-content',
+            questions: [
+                {id: 1, text: "Data", isCorrect: false},
+                {id: 2, text: "Data", isCorrect: false},
+                {id: 3, text: "Data", isCorrect: true},
+                {id: 4, text: "Data", isCorrect: false},
+                {id: 5, text: "Data", isCorrect: true},
+            ],
+        },
+        {
+            id: 2,
+            title: 'Второй вопрос',
+            content: 'Second-content',
+            questions: [
+                {id: 1, text: "Data", isCorrect: false},
+                {id: 2, text: "Data", isCorrect: false},
+                {id: 3, text: "Data", isCorrect: true},
+                {id: 4, text: "Data", isCorrect: false},
+                {id: 5, text: "Data", isCorrect: true},
+            ]
+        },
+        {
+            id: 3,
+            title: 'Второй вопрос',
+            content: 'Second-content',
+            questions: [
+                {id: 1, text: "Data", isCorrect: false},
+                {id: 2, text: "Data", isCorrect: false},
+                {id: 3, text: "Data", isCorrect: true},
+                {id: 4, text: "Data", isCorrect: false},
+                {id: 5, text: "Data", isCorrect: true},
+            ],
+        },
+        {
+            id: 4,
+            title: 'Второй вопрос',
+            content: 'Second-content',
+            questions: [
+                {id: 1, text: "Data", isCorrect: false},
+                {id: 2, text: "Data", isCorrect: false},
+                {id: 3, text: "Data", isCorrect: true},
+                {id: 4, text: "Data", isCorrect: false},
+                {id: 5, text: "Data", isCorrect: true},
+            ],
+        },
+        {
+            id: 5,
+            title: 'Второй вопрос',
+            content: 'Second-content',
+            questions: [
+                {id: 1, text: "Data", isCorrect: false},
+                {id: 2, text: "Data", isCorrect: false},
+                {id: 3, text: "Data", isCorrect: true},
+                {id: 4, text: "Data", isCorrect: false},
+                {id: 5, text: "Data", isCorrect: true},
+            ],
+        }
+    ]
+}
+
+export default testData;
